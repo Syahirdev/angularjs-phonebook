b6b93915c079da782914ef30fcf39967d2b1c09a
Syahir Dev <888syahir@gmail.com>
2020-09-22 19:27:04 +0800

    directives
    attribute directives
    $ classes = { blue: true, red: false, underline: true };
    $ <p [ngClass]="classes">directory works!</p>
    
    structural directives
    1. ngIf (If Statement)
    $ test = true;
    $ <p *ngIf="test">Only show if test is true</p>
    
    2. ngFor (Looping)
    $ <li *ngFor="let person of persons">
          <h1>{{ person.name }}</h1>
      </li>

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -7,6 +7,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./directory.component.css'],
 })
 export class DirectoryComponent implements OnInit {
+  classes = { blue: true, red: false, underline: true };
+  test = true;
+  persons = [
+    {
+      name: 'Syahir',
+      phoneNo: '012',
+    },
+    {
+      name: 'Ahmad',
+      phoneNo: '013',
+    },
+    {
+      name: 'Ali',
+      phoneNo: '014',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
