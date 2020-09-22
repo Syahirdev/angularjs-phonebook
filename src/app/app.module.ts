383f2c724ba10bd06c6f62d7d2f5613737d7934e
Syahir Dev <888syahir@gmail.com>
2020-09-22 18:36:04 +0800

    routing
    adds new component : directory
    
    1. create new app.routes.ts file
    2. declare the path in the routes files
    3. export to app.module.ts
    4. using $ <router-outlet></router-outlet> in root component (app.component.html)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DirectoryComponent } from './directory/directory.component';
+
+import { Routing } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HomeComponent, DirectoryComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, Routing],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { DirectoryComponent } from './directory/directory.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'directory',
+    component: DirectoryComponent,
+  },
+];
+export const Routing = RouterModule.forRoot(routes);
